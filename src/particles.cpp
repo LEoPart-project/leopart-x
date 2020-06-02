2ec88736c54a82a7dc3c3a14bcb1de717d0aa800
Chris Richardson <chris@bpi.cam.ac.uk>
2020-06-02 12:57:36 +0100

    Fix cols to 3

--- src/particles.cpp
+++ src/particles.cpp
@@ -5,7 +5,9 @@ using namespace leopart;
 
 particles::particles(
     const Eigen::Array<double, Eigen::Dynamic, 3, Eigen::RowMajor>& x,
-    const std::vector<int>& cells) : _field_name({"x"}), _field_shape({{3}}), _field_data({std::vector<double>(x.rows()* x.cols())})
+    const std::vector<int>& cells)
+    : _field_name({"x"}), _field_shape({{3}}),
+      _field_data({std::vector<double>(x.rows() * 3)})
 {
   // Find max cell index, and create cell->particle map
   auto max_cell_it = std::max_element(cells.begin(), cells.end());
@@ -17,5 +19,5 @@ particles::particles(
     _cell_particles[cells[p]].push_back(p);
 
   // Copy position data to first field
-  std::copy(x.data(), x.data() + x.rows() * x.cols(), _field_data[0].begin());
+  std::copy(x.data(), x.data() + x.rows() * 3, _field_data[0].begin());
 }
