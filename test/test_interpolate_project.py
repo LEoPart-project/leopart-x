a23ef40acf32e01d2742f3bbd08d7a8cb678cac5
Jakob Maljaars <jakobmaljaars@gmail.com>
2020-08-08 09:14:01 +0200

    Split tests

--- test/test_interpolate_project.py
+++ test/test_interpolate_project.py
@@ -4,36 +4,26 @@ import numpy as np
 import pyleopart
 import pytest
 
-# Test back-and-forth projection on 2D mesh
-@pytest.mark.parametrize("is_vec, k", [(True, 2), (True, 3), (False, 2), (False, 3)])
-def test_interpolate_project_dg(is_vec, k):
+# Test back-and-forth projection on 2D mesh: scalar case
+@pytest.mark.parametrize("k", [2, 3])
+def test_interpolate_project_dg_scalar(k):
     npart = 20
     mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 5, 5)
     ncells = mesh.topology.index_map(2).size_local
     x, c = pyleopart.mesh_fill(mesh, ncells * npart)
     p = pyleopart.Particles(x, c)
 
-    Q = (
-        dolfinx.function.VectorFunctionSpace(mesh, ("DG", k))
-        if is_vec
-        else dolfinx.function.FunctionSpace(mesh, ("DG", k))
-    )
+    Q = dolfinx.function.FunctionSpace(mesh, ("DG", k))
+
     pbasis = pyleopart.get_particle_contributions(p, Q._cpp_object)
 
-    if is_vec:
-
-        def sq_val(x):
-            return [x[0] ** k, x[1] ** k]
-
-    else:
-
-        def sq_val(x):
-            return x[0] ** k
+    def sq_val(x):
+        return x[0] ** k
 
     u = dolfinx.function.Function(Q)
     u.interpolate(sq_val)
 
-    p.add_field("v", [2]) if is_vec else p.add_field("v", [1])
+    p.add_field("v", [1])
     v = p.field("v")
 
     # Transfer from Function "u" to field "v"
@@ -42,14 +32,50 @@ def test_interpolate_project_dg(is_vec, k):
     # Transfer from field "v" back to Function "u"
     pyleopart.transfer_to_function(u._cpp_object, p, v, pbasis)
 
-    p.add_field("w", [2]) if is_vec else p.add_field("w", [1])
+    p.add_field("w", [1])
     w = p.field("w")
     # Transfer from Function "u" to field "w"
     pyleopart.transfer_to_particles(p, w, u._cpp_object, pbasis)
 
     # Compare fields "w" and "x"(squared)
     for pidx in range(len(x)):
-        expected = (
-            p.field("x").data(pidx) ** k if is_vec else p.field("x").data(pidx)[0] ** k
-        )
+        expected = p.field("x").data(pidx)[0] ** k
+        assert np.isclose(p.field("w").data(pidx), expected).all()
+
+
+# Test back-and-forth projection on 2D mesh: vector valued case
+@pytest.mark.parametrize("k", [2, 3])
+def test_interpolate_project_dg_vector(k):
+    npart = 20
+    mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 5, 5)
+    ncells = mesh.topology.index_map(2).size_local
+    x, c = pyleopart.mesh_fill(mesh, ncells * npart)
+    p = pyleopart.Particles(x, c)
+
+    Q = dolfinx.function.VectorFunctionSpace(mesh, ("DG", k))
+    pbasis = pyleopart.get_particle_contributions(p, Q._cpp_object)
+
+    def sq_val(x):
+        return [x[0] ** k, x[1] ** k]
+
+    u = dolfinx.function.Function(Q)
+    u.interpolate(sq_val)
+
+    p.add_field("v", [2])
+    v = p.field("v")
+
+    # Transfer from Function "u" to field "v"
+    pyleopart.transfer_to_particles(p, v, u._cpp_object, pbasis)
+
+    # Transfer from field "v" back to Function "u"
+    pyleopart.transfer_to_function(u._cpp_object, p, v, pbasis)
+
+    p.add_field("w", [2])
+    w = p.field("w")
+    # Transfer from Function "u" to field "w"
+    pyleopart.transfer_to_particles(p, w, u._cpp_object, pbasis)
+
+    # Compare fields "w" and "x"(squared)
+    for pidx in range(len(x)):
+        expected = p.field("x").data(pidx) ** k
         assert np.isclose(p.field("w").data(pidx), expected).all()
