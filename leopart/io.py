d94f1812da46cd684efb56f43bba48a50916b6bf
nate-sime <nsime@carnegiescience.edu>
2023-11-23 12:00:01 -0500

    add copyright

--- leopart/io.py
+++ leopart/io.py
@@ -1,3 +1,8 @@
+# Copyright: (c) 2023 Jørgen Dokken and Nathan Sime
+# This file is part of LEoPart-X, a particle-in-cell package for DOLFIN-X
+# License: GNU Lesser GPL version 3 or any later version
+# SPDX-License-Identifier:    LGPL-3.0-or-later
+
 import typing
 
 from mpi4py import MPI
