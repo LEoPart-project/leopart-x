a00cf15b876dbd1a2ce0edb254f320240eeb9532
nate-sime <nsime@carnegiescience.edu>
2023-11-30 12:19:44 -0500

    io test

---
+++ test/test_io.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2023 Nathan Sime
+# This file is part of LEoPart-X, a particle-in-cell package for DOLFIN-X
+# License: GNU Lesser GPL version 3 or any later version
+# SPDX-License-Identifier:    LGPL-3.0-or-later
+
+import numpy as np
+import pytest
+import dolfinx
+from mpi4py import MPI
+import adios2
+import pathlib
+import leopart.io
+import leopart.cpp as pyleopart
+
+
+def test_xdmf_output(tmpdir):
+    mesh = dolfinx.mesh.create_rectangle(
+        MPI.COMM_WORLD, [[-1.0, -1.0], [1.0, 1.0]], [32, 32],
+        cell_type=dolfinx.mesh.CellType.triangle)
+    x, p2c = pyleopart.mesh_fill(mesh._cpp_object, 25)
+    x = np.c_[x, np.zeros_like(x[:, 0])]
+    ptcls = pyleopart.Particles(x, p2c)
+
+    filename = pathlib.Path(tmpdir) / "example.xdmf"
+    fi = leopart.io.XDMFParticlesFile(
+        MPI.COMM_WORLD, filename, adios2.Mode.Write)
+
+    shapes = (1, 2, 3)
+    field_names = [f"field{shape}" for shape in shapes]
+    for shape, field_name in zip(shapes, field_names):
+        ptcls.add_field(field_name, [shape])
+
+    for t in [0.0, 1.0]:
+        fi.write_particles(ptcls, t, field_names)
