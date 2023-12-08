c35921e4249904b877c13081f64017516d2f9046
nate-sime <nsime@carnegiescience.edu>
2023-12-08 15:43:15 -0500

    some documentation and rearranging of demo

--- demo/demo_mantle-convection/demo_mantle-convection.py
+++ demo/demo_mantle-convection/demo_mantle-convection.py
@@ -4,15 +4,16 @@
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
 from mpi4py import MPI
+from petsc4py import PETSc
 
+import adios2
 import numpy as np
 
 import dolfinx
+import dolfinx.fem.petsc
 import leopart.cpp as pyleopart
-import ufl
-from petsc4py import PETSc
-
 import leopart.io
+import ufl
 
 
 def pprint(*msg, rank=None):
@@ -36,12 +37,6 @@ tableau = pyleopart.tableaus.order2.explicit_midpoint()
 t_max = 2000.0
 num_t_steps_max = 200
 
-xp, p2cell = pyleopart.mesh_fill(mesh._cpp_object, 30)
-xp = np.c_[xp, np.zeros_like(xp[:, 0])]
-pprint(f"num paticles: {xp.shape[0]}")
-ptcls = pyleopart.Particles(xp, p2cell)
-tableau.check_and_create_fields(ptcls)
-
 # Chemistry space
 PHI = dolfinx.fem.FunctionSpace(mesh, ("DG", 0))
 phi = dolfinx.fem.Function(PHI)
@@ -49,9 +44,19 @@ phi.interpolate(lambda x: np.where(x[1] > db, 1.0, 0.0))
 
 # Perturb the mesh and initial condition
 meshx = mesh.geometry.x
-meshx[:,1] += meshx[:,1] * (H - meshx[:,1]) / S \
-              * A * np.cos(np.pi*lmbda * meshx[:,0])
+meshx[:, 1] += meshx[:, 1] * (H - meshx[:, 1]) / S \
+              * A * np.cos(np.pi * lmbda * meshx[:, 0])
 
+# Generate particles and interpolate the composition
+xp, p2cell = pyleopart.mesh_fill(mesh._cpp_object, 30)
+xp = np.c_[xp, np.zeros_like(xp[:, 0])]
+pprint(f"num paticles: {xp.shape[0]}")
+ptcls = pyleopart.Particles(xp, p2cell)
+tableau.check_and_create_fields(ptcls)
+ptcls.add_field("phi", [1])
+pyleopart.transfer_to_particles(ptcls, ptcls.field("phi"), phi._cpp_object)
+
+# Viscosity model
 eta_top = dolfinx.fem.Constant(mesh, 1.0)
 eta_bottom = dolfinx.fem.Constant(mesh, 0.1)
 mu = eta_bottom + phi * (eta_top - eta_bottom)
@@ -76,7 +81,7 @@ Rb = dolfinx.fem.Constant(mesh, 1.0)
 f = Rb * phi * ufl.as_vector((0, -1))
 L = ufl.inner(f, v) * ufl.dx
 
-
+# Create BCs: free slip on left and right, zero flow top and bottom
 facets_top_bot = dolfinx.mesh.locate_entities_boundary(
     mesh, dim=mesh.topology.dim - 1,
     marker=lambda x: np.isclose(x[1], 0.0) | np.isclose(x[1], H))
@@ -90,10 +95,10 @@ dofs_lr = dolfinx.fem.locate_dofs_topological(
     (W.sub(0).sub(0), V_x[0]), mesh.topology.dim - 1, facets_left_right)
 zero_x_bc = dolfinx.fem.dirichletbc(zero, dofs_lr, W.sub(0).sub(0))
 
-V_y = W.sub(0).collapse()
-zero = dolfinx.fem.Function(V_y[0])
+W0 = W.sub(0).collapse()
+zero = dolfinx.fem.Function(W0[0])
 dofs_tb = dolfinx.fem.locate_dofs_topological(
-    (W.sub(0), V_y[0]), mesh.topology.dim - 1, facets_top_bot)
+    (W.sub(0), W0[0]), mesh.topology.dim - 1, facets_top_bot)
 zero_y_bc = dolfinx.fem.dirichletbc(zero, dofs_tb, W.sub(0))
 
 # Pin pressure DoF in bottom left corner for solvable system
@@ -104,45 +109,42 @@ dofs_p = dolfinx.fem.locate_dofs_geometrical(
     lambda x: np.isclose(x[0], 0.0) & np.isclose(x[1], 0.0))
 zero_p_bc = dolfinx.fem.dirichletbc(zero_p, dofs_p, W.sub(1))
 
-import dolfinx.fem.petsc
 problem = dolfinx.fem.petsc.LinearProblem(
     a, L, u=Uh, bcs=[zero_x_bc, zero_y_bc, zero_p_bc],
     petsc_options={"ksp_type": "preonly", "pc_type": "lu",
                    "pc_factor_mat_solver_type": "mumps"})
 
-ptcls.add_field("phi", [1])
-pyleopart.transfer_to_particles(ptcls, ptcls.field("phi"), phi._cpp_object)
-
+# Velocity as function of time to be used in Runge-Kutta integration
 uh = Uh.sub(0)
 def velocity(t):
     pyleopart.transfer_to_function(phi._cpp_object, ptcls, ptcls.field("phi"))
     problem.solve()
     return uh._cpp_object
 
-
+# h measured used in CFL criterion estimation
 h_measure = dolfinx.cpp.mesh.h(
     mesh._cpp_object, mesh.topology.dim - 1, np.arange(
         mesh.topology.index_map(mesh.topology.dim - 1).size_local,
         dtype=np.int32))
 hmin = mesh.comm.allreduce(h_measure.min(), op=MPI.MIN)
 
-problem.solve()
-
-t = 0.0
-import adios2
+# Output files
 ptcl_file = leopart.io.XDMFParticlesFile(
     mesh.comm, "particles.xdmf", adios2.Mode.Write)
-ptcl_file.write_particles(ptcls, t, ["phi"])
-
 phi_file = dolfinx.io.XDMFFile(mesh.comm, "phi.xdmf", "w")
-phi_file.write_mesh(mesh)
-phi_file.write_function(phi, t=t)
 
+# Space for estimating speed for CFL criterion
 Vspd = dolfinx.fem.FunctionSpace(mesh, ("DG", 0))
 uspd = dolfinx.fem.Function(Vspd)
 uspd_expr = dolfinx.fem.Expression(
     ufl.sqrt(ufl.inner(uh, uh)), Vspd.element.interpolation_points())
 
+# Main time loop
+t = 0.0
+ptcl_file.write_particles(ptcls, t, ["phi"])
+phi_file.write_mesh(mesh)
+phi_file.write_function(phi, t=t)
+problem.solve()  # Get initial speed estimate
 for j in range(num_t_steps_max):
     uspd.interpolate(uspd_expr)
     uspd.vector.assemble()
